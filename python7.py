de0624c95fd4b305b65fefb2b5b4f2498dc1f77e
7-MAX25 <107028520+7-MAX25@users.noreply.github.com>
2022-06-07 09:59:12 +0200

    Add files via upload

---
+++ python7.py
@@ -0,0 +1,28 @@
+mport os, sys, time
+
+def jalan (z):
+        for e in z +'\n':
+                sys.stdout.write(e)
+                sys.stdout.flush ()
+                time.sleep (00000.03)
+
+print ("\033[1;31m")
+os.system ('figlet Ahmed')
+
+def list():
+        print ("[1] ALIENS.HaCK.Camera")
+        print ("[2] Storm-Breaker")
+        print ("[3] LEDEAR_HACKING")
+        print ("[4] Ddos404")
+        print ("[5] sqlmap-dev")
+list()
+print(" ")
+Choose = input(" [«] Choose :") 
+if choose == '1':
+	os.system("git clone https://github.com/hackermohamedPro/ALIENS.HaCK.Camera;cd ALIENS.HaCK.Camera; bash alienshack.sh") 
+	os.system("python3 python.py") 
+	
+
+	
+	 
+
